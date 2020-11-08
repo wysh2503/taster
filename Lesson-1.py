f11f037540cbded7b245603f15b519f0f7331c04
Winston Yeung <74127616+wysh2503@users.noreply.github.com>
2020-11-08 14:19:01 +0800

    Add files via upload

---
+++ Lesson-1.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+print(cv2.__version__)
+
+# Set up webcam
+capture = cv2.VideoCapture(1)
+capture.set(3,640)
+capture.set(4,480)
+
+# Start capturing and show frames on window
+while True:
+    success, img = capture.read()
+    cv2.imshow('Frame', img)
+    cv2.moveWindow('Frame', 100,20)
+    if cv2.waitKey(20) & 0xff == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
