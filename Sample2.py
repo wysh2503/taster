16116a584bc9a9dbd3f92cc58f8a7e2239507aa2
Winston Yeung <74127616+wysh2503@users.noreply.github.com>
2021-04-23 20:51:04 +0800

    Update and rename Lesson-1.py to Sample2.py

--- Sample2.py
+++ Sample2.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 print(cv2.__version__)
 
-# Set up webcam
+# Set up webcam. If no frame is shown, try (0) or (2)
 capture = cv2.VideoCapture(1)
 capture.set(3,640)
 capture.set(4,480)
