63c7459758fb3d4b9899eea000dea7192ea36f4f
Winston Yeung <74127616+wysh2503@users.noreply.github.com>
2021-04-23 21:17:23 +0800

    Update Sample1.py

--- Sample1.py
+++ Sample1.py
@@ -1,10 +1,8 @@
-# Read a static image and stream frames from webcam
+# Read a static image and test on different image processing
 import cv2
 import numpy as np
 print(cv2.__version__)
 
-#------------------------------------------------------------------
-# This part reads static image and test on different image processing
 kernel = np.ones((5,5),np.uint8)
 
 img = cv2.imread('Resources/lena.png')
@@ -24,21 +22,6 @@ cv2.imshow('Lena',img)
 # cv2.imshow('Dilate', imgDilation)
 
 cv2.waitKey(0)
-#------------------------------------------------------------------
-# This part stream frames from webcam
-# Read image from webcam
-# capture = cv2.VideoCapture(1)
-# capture.set(3,640)
-# capture.set(4,480)
-#
-# while True:
-#     success, img = capture.read()
-#     cv2.imshow('Frame', img)
-#     cv2.moveWindow('Frame', 100,20)
-#     if cv2.waitKey(20) & 0xff == ord('q'):
-#         break
-#
-# capture.release()
-# cv2.destroyAllWindows()
+
 
 
