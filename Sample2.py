0001a09cf19706553ab84604f1f1af38443be493
Winston Yeung <74127616+wysh2503@users.noreply.github.com>
2021-05-19 20:55:08 +0800

    Update Sample2.py

--- Sample2.py
+++ Sample2.py
@@ -4,17 +4,16 @@ import numpy as np
 print(cv2.__version__)
 
 # Set up webcam. If no frame is shown, try (0) or (2)
-capture = cv2.VideoCapture(1)
-capture.set(3,640)
-capture.set(4,480)
+cam = cv2.VideoCapture(1)
+# cam.set(3,640)
+# cam.set(4,480)
 
 # Start capturing and show frames on window
 while True:
-    success, img = capture.read()
+    success, img = cam.read()
     cv2.imshow('Frame', img)
-#     cv2.moveWindow('Frame', 100,20)
-    if cv2.waitKey(20) & 0xff == ord('q'):
+    if cv2.waitKey(1) & 0xff == 27:
         break
 
-capture.release()
+cam.release()
 cv2.destroyAllWindows()
