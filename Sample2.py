57d8399269db01a0f3de1f344098208641a46cc4
Winston Yeung <74127616+wysh2503@users.noreply.github.com>
2021-05-19 21:06:04 +0800

    Update Sample2.py

--- Sample2.py
+++ Sample2.py
@@ -1,12 +1,9 @@
 # Below code stream frames from webcam and display it on 'Frame'
 import cv2
-import numpy as np
 print(cv2.__version__)
 
 # Set up webcam. If no frame is shown, try (0) or (2)
 cam = cv2.VideoCapture(1)
-# cam.set(3,640)
-# cam.set(4,480)
 
 # Start capturing and show frames on window
 while True:
