e22ae2d2d3b6fd57a1c7e27d3f4c912d4e813afe
Winston Yeung <74127616+wysh2503@users.noreply.github.com>
2021-05-19 20:57:03 +0800

    Update Sample4.py

--- Sample4.py
+++ Sample4.py
@@ -5,21 +5,20 @@ print(cv2.__version__)
 
 faceCascade = cv2.CascadeClassifier('Resources/haarcascade_frontalface_default.xml')
 
-capture = cv2.VideoCapture(1)
-capture.set(3,640)
-capture.set(4,480)
+cam = cv2.VideoCapture(1)
+# cam.set(3,640)
+# cam.set(4,480)
 
 while True:
-    success, img = capture.read()
+    success, img = cam.read()
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(imgGray, 1.1, 4)
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
     cv2.imshow('Frame', img)
-    cv2.moveWindow('Frame', 100,20)
-    if cv2.waitKey(20) & 0xff == ord('q'):
+    if cv2.waitKey(1) & 0xff == 27:
         break
 
-capture.release()
+cam.release()
 cv2.destroyAllWindows()
